aac0e32c47d1336b7129301f2d4d2a3e0b03501d
bdaneshgar <brian@briandaneshgar.com>
2025-10-07 05:59:54 +0000

    changes

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -2,16 +2,6 @@ $(document).ready(function () {
 
     /***************** Waypoints ******************/
 
-    $('.wp1').waypoint(function () {
-        $('.wp1').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
-    });
-    $('.wp2').waypoint(function () {
-        $('.wp2').addClass('animated fadeInRight');
-    }, {
-        offset: '75%'
-    });
     $('.wp3').waypoint(function () {
         $('.wp3').addClass('animated fadeInLeft');
     }, {
@@ -34,16 +24,6 @@ $(document).ready(function () {
     });
     $('.wp7').waypoint(function () {
         $('.wp7').addClass('animated fadeInUp');
-    }, {
-        offset: '75%'
-    });
-    $('.wp8').waypoint(function () {
-        $('.wp8').addClass('animated fadeInLeft');
-    }, {
-        offset: '75%'
-    });
-    $('.wp9').waypoint(function () {
-        $('.wp9').addClass('animated fadeInRight');
     }, {
         offset: '75%'
     });
